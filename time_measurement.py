cb05b7b44b25682293542d16c0810b445369c81a
frezario <91615650+frezario@users.noreply.github.com>
2022-02-20 15:08:19 +0200

    Update time_measurement.py
    Added functionality to measure time and build graphs

--- time_measurement.py
+++ time_measurement.py
@@ -1,22 +1,85 @@
+'''
+    A module for time measurement of algorithms.
+'''
+from telnetlib import COM_PORT_OPTION
 import time
-import create_graph
-from networkx.algorithms import tree
 from tqdm import tqdm
+import create_graph
+import kruskal_algorithm
+import prim_algorithm
 
 
-NUM_OF_ITERATIONS = 1000
+def measure_avarage_time(algorithm, num_of_iterations: int, nodes_count: int, completeness: float):
+    time_taken = 0
+    for _ in tqdm(range(num_of_iterations)):
+        graph = create_graph.gnp_random_connected_graph(
+            nodes_count, completeness, False)
+        start = time.time()
+        algorithm(graph)
+        end = time.time()
+        time_taken += end - start
+    return time_taken / num_of_iterations
 
 
-time_taken = 0
-for i in tqdm(range(NUM_OF_ITERATIONS)):
+def show_difference():
+    print('Pls, enter the number of iterations to perform:')
+    while True:
+        try:
+            num_of_iterations = int(input('>>> '))
+            break
+        except ValueError:
+            print("You've made a mistake. Please, enter a correct value:")
 
-    # note that we should not measure time of graph creation
-    G = create_graph.gnp_random_connected_graph(100, 0.1, False)
+    print("Now enter the number of graph nodes and it's competeness (separated by space):")
+    while True:
+        try:
+            nodes_count, completeness = input('>>> ').split()
+            nodes_count = int(nodes_count)
+            completeness = float(completeness)
+            assert 0 <= completeness <= 1
+            break
+        except (ValueError, AssertionError):
+            print("You've made a mistake. Please, enter a correct value:")
 
-    start = time.time()
-    tree.minimum_spanning_tree(G, algorithm="prim")
-    end = time.time()
+    print('Executing Kruskal algorithm:')
+    av_time1 = measure_avarage_time(
+        kruskal_algorithm.kruskal_algorithm, num_of_iterations, nodes_count, completeness)
+    print(f'Average time per iteration: {av_time1}')
+    print('Executing Prim algorithm:')
+    av_time2 = measure_avarage_time(
+        prim_algorithm.prim_algorithm, num_of_iterations, nodes_count, completeness)
+    print(f'Average time per iteration: {av_time2}')
+    return num_of_iterations, completeness
 
-    time_taken += end - start
 
-time_taken / NUM_OF_ITERATIONS
+def plot_statistics(iterations:int = 15, completeness:int = 0.5):
+    print('Would you like to plot a graph with a statistics using pre-entered data?(Y/n)')
+    answer = input('>>> ')[0]
+    if answer in 'Nn':
+        return None
+    print('Graphing...')
+    import matplotlib.pyplot as plt
+    x_axes = list(range(50, 300, 50))
+    # Kruskal
+    kruskal_axes = [measure_avarage_time(kruskal_algorithm.kruskal_algorithm, iterations, nodes_count, completeness)
+              for nodes_count in x_axes]
+    plt.plot(x_axes, kruskal_axes, label = "Kruskal")
+    prim_axes = [measure_avarage_time(prim_algorithm.prim_algorithm, iterations, nodes_count, completeness)
+              for nodes_count in x_axes]
+    plt.plot(x_axes, prim_axes, label = "Prim")
+
+    plt.xlabel('x - number of nodes')
+    plt.ylabel('y - average time')
+    
+    plt.title(f'A graph comparison of two algorithms ({iterations} iterations, {completeness} completeness)')
+
+    plt.legend()
+    plt.show()
+
+
+def main():
+    num_of_iterations, completeness = show_difference()
+    plot_statistics(num_of_iterations, completeness)
+
+if __name__ == '__main__':
+    main()

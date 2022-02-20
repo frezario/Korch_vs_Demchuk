3bbe38c807aafad5dd747b8f8a0099fc8a8a8666
frezario <91615650+frezario@users.noreply.github.com>
2022-02-20 12:31:51 +0200

    Update time_measurement.py

--- time_measurement.py
+++ time_measurement.py
@@ -1,5 +1,5 @@
 import time
-import create_craph
+import create_graph
 from networkx.algorithms import tree
 from tqdm import tqdm
 
@@ -11,7 +11,7 @@ time_taken = 0
 for i in tqdm(range(NUM_OF_ITERATIONS)):
 
     # note that we should not measure time of graph creation
-    G = create_craph.gnp_random_connected_graph(100, 0.1, False)
+    G = create_graph.gnp_random_connected_graph(100, 0.1, False)
 
     start = time.time()
     tree.minimum_spanning_tree(G, algorithm="prim")
